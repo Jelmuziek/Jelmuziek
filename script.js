f16034410d94859295074635da5c435c0593b50a
Huub <Huubboekhorst@icloud.com>
2026-03-16 00:45:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function toggleMenu(){
     const menu = document.getElementById("navlinks");
     menu.classList.toggle("active");
     }
+
+window.onscroll = function() {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.querySelector(".scrollbar").style.width = scrolled + "%";
+};
